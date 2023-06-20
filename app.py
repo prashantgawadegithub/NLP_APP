dce73c07e4aa4ac3fc9df464289bea2da054ecfa
Prashant Gawade <gawadeprashant97@gmail.com>
2023-06-20 11:19:24 +0530

    add login title NLPApp

---
+++ app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class NLPApp:
+
+    def __init__(self):
+
+        #login gui
+        self.root=Tk()
+        self.root.title('NLPApp')
+        self.root.mainloop()
+
+nlp = NLPApp()
