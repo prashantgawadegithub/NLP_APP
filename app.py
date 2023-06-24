083296024624e680450c2e15ee20107a0918462e
Prashant Gawade <gawadeprashant97@gmail.com>
2023-06-24 14:24:43 +0530

    add image favicon

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ class NLPApp:
         #login gui
         self.root=Tk()
         self.root.title('NLPApp')
+        self.root.iconbitmap('resources/favicon.ico')
+        self.root.geometry('350x600')
+        self.root.configure(bg='#FFE933')
+        self.login_gui()
         self.root.mainloop()
+
+    def login_gui(self):
+        heading=Label(self.root,text="NLPApp")
+
+
 
 nlp = NLPApp()
